be80a8b8a0095423a4725efd92cb038490b61135
adollinger@edenschool.fr <adollinger@edenschool.fr>
2024-10-05 11:14:49 +0200

    transfered to scss and jquery, all working

--- main.js
+++ main.js
@@ -1,64 +1,75 @@
-// reaction time 
-let startTime = 0;
-let clickTime = 0;
-let timeText = document.getElementById('reactionTimeText')
+// reaction time
+const $timeText = $('#reactionTimeText');
+
+$('#reactionTime').on('click', reactionTime);
 
 function reactionTime() {
-    timeText.innerText = ""
-    document.getElementById('reactionTime').removeAttribute('onclick')
-    timer()
+    $timeText.text("");
+    $('#reactionTime').off('click');
+    timer();
 }
 
 function stopWatch() {
-    let time = 0
+    let time = 0;
     const stopWatchInterval = setInterval(() => {
-        time++
-        console.log(time)
+        time++;
+        console.log(time);
     }, 10);
-    addEventListener('click', clickEvent = function () {
-        clearInterval(stopWatchInterval)
-        showTime(time)
-        reactionTimeScore(time)
-        removeEventListener('click', clickEvent)
-    })
+    const clickEvent = function () {
+        clearInterval(stopWatchInterval);
+        showTime(time);
+        reactionTimeScore(time);
+        $(document).off('click', clickEvent);
+    };
+    $(document).on('click', clickEvent);
 }
+
 function timer() {
-    let timeLeft = Math.floor(Math.random() * 300 + 200)
+    let timeLeft = Math.floor(Math.random() * 300 + 200);
     const timerInterval = setInterval(() => {
-        timeLeft <= 0 ? (clearInterval(timerInterval), changeColor()) : timeLeft--
-        console.log(timeLeft)
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            changeColor();
+        } else {
+            timeLeft--;
+        }
+        console.log(timeLeft);
     }, 10);
 }
+
 function changeColor() {
-    console.log("hello")
-    document.getElementById('reactionTimeColor').style.backgroundColor = '#00CCDD'; 
+    console.log("hello");
+    $('#reactionTimeColor').css('background-color', '#00CCDD');
     stopWatch();
-} 
+}
+
 function showTime(n) {
-    document.getElementById('reactionTimeColor').style.color = '#6439FF';
-    (n+'').length == 1 ? timeText.innerText = `0.0${n}s` : (n+'').length == 2 ? timeText.innerText = `0.${n}s` : (n+'').length == 3 ? timeText.innerText = `${(n+'')[0]}.${(n+'')[1]}${(n+'')[2]}s` : timeText.innerText = "You can't be that slow"
-    document.getElementById('reactionTime').setAttribute('onclick', 'restartReactionTime()')
+    $('#reactionTimeColor').css('color', '#6439FF');
+    if (n < 10) {
+        $timeText.text(`0.0${n}s`);
+    } else if (n < 100) {
+        $timeText.text(`0.${n}s`);
+    } else if (n < 1000) {
+        $timeText.text(`${Math.floor(n / 100)}.${n % 100}s`);
+    } else {
+        $timeText.text("You can't be that slow");
+    }
+    $('#reactionTime').on('click', restartReactionTime);
 }
+
 function restartReactionTime() {
-    location.reload()
+    console.log('no')
+    location.reload();
 }
+
 function reactionTimeScore(n) {
-    let reactionTimeScores = {
-    }
+    let reactionTimeScores = {};
     if (sessionStorage.getItem('reactionTimeScore')) {
-        let length = []
-        reactionTimeScores = JSON.parse(sessionStorage.getItem('reactionTimeScore'))
-        for (const key in reactionTimeScores) {
-            length.push(key)
-            console.log(reactionTimeScores[key])
-        }
-        console.log(reactionTimeScores)
-        reactionTimeScores[length.length + 1] = n
-        sessionStorage.setItem('reactionTimeScore', JSON.stringify(reactionTimeScores))
+        reactionTimeScores = JSON.parse(sessionStorage.getItem('reactionTimeScore'));
+        reactionTimeScores[Object.keys(reactionTimeScores).length + 1] = n;
+    } else {
+        console.log(n)
+        reactionTimeScores[1] = n;
     }
-    else {
-        reactionTimeScores[1] = n
-        console.log(reactionTimeScores)
-        sessionStorage.setItem('reactionTimeScore', JSON.stringify(reactionTimeScores))
-    }
-} 
+    sessionStorage.setItem('reactionTimeScore', JSON.stringify(reactionTimeScores));
+}
